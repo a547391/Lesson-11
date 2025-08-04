bea0364e1e44e210e81d931c35485f15de689d84
joyce <a547395@gmail.com>
2025-08-04 14:16:04 +0800

    P107-108

--- index_s01.php
+++ index_s01.php
@@ -264,21 +264,21 @@
                     <?php $i++;
                     } ?>
                     <div class="row mt-2">
-<?php    //取得目前頁數
-if (isset($_GET['totalRows_rs'])) {
-    $totalRows_rs = $_GET['totalRows_rs'];
-} else {
-    $all_rs = $link->query($queryFirst);
-    $totalRows_rs = $all_rs->rowCount();
-}
-$totalPages_rs = ceil($totalRows_rs / $maxRows_rs) - 1;
-//呼叫分頁功能函數
-$prev_rs = "&laquo;";
-$next_rs = "&raquo;";
-$separator = " | ";
-$max_links = 20;
-$pages_rs = buildNavigation($pageNum_rs, $totalPages_rs, $prev_rs, $next_rs, $separator, $max_links, true, 3, "rs");
-?>
+                        <?php    //取得目前頁數
+                        if (isset($_GET['totalRows_rs'])) {
+                            $totalRows_rs = $_GET['totalRows_rs'];
+                        } else {
+                            $all_rs = $link->query($queryFirst);
+                            $totalRows_rs = $all_rs->rowCount();
+                        }
+                        $totalPages_rs = ceil($totalRows_rs / $maxRows_rs) - 1;
+                        //呼叫分頁功能函數
+                        $prev_rs = "&laquo;";
+                        $next_rs = "&raquo;";
+                        $separator = " | ";
+                        $max_links = 20;
+                        $pages_rs = buildNavigation($pageNum_rs, $totalPages_rs, $prev_rs, $next_rs, $separator, $max_links, true, 3, "rs");
+                        ?>
                         <nav aria-label="Page navigation example">
                             <ul class="pagination justify-content-center">
                                 <?php echo $pages_rs[0] . $pages_rs[1] . $pages_rs[2]; ?>
@@ -348,6 +348,7 @@ $pages_rs = buildNavigation($pageNum_rs, $totalPages_rs, $prev_rs, $next_rs, $se
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="gotop.js"></script>
 </body>
 
 </html>
